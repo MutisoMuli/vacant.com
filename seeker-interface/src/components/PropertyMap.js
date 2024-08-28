34dd629824ad629df76bd0e82aba0683c35c5611
Mutiso Muli <mulimutiso525@gmail.com>
2024-08-28 10:03:49 +0300

    updated PropertyMap.js

--- seeker-interface/src/components/PropertyMap.js
+++ seeker-interface/src/components/PropertyMap.js
@@ -1,3 +1,4 @@
+//PropertyMap.js
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Map, { Marker, NavigationControl, ScaleControl, GeolocateControl } from 'react-map-gl';
