6b5d7baf58f37b98dbdc77456e933bd6283c247a
Mutiso Muli <mulimutiso525@gmail.com>
2024-08-28 10:16:19 +0300

    updated src/redux/store.js

--- seeker-interface/src/redux/store.js
+++ seeker-interface/src/redux/store.js
@@ -1,4 +1,4 @@
-// src/redux/store.js
+// src redux store.js
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
